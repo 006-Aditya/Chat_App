ca8a123d8388a9d3126e704c19668fcadc3ba1dc
Aditya kumar <aditya512001@gmail.com>
2025-10-29 03:36:49 +0530

    update profile

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,17 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import {connectDB} from "./lib/db.js";
+import {ENV} from "./lib/env.js";
 
 dotenv.config();
 const app=express();
-const PORT=process.env.PORT || 3000;
+const PORT=ENV.PORT || 3000;
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
