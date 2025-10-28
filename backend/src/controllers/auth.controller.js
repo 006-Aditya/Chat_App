ed16f9fa28f45d074dbe83e6eb61ef7aa116a4f3
Aditya kumar <aditya512001@gmail.com>
2025-10-29 03:04:21 +0530

    login

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -72,6 +72,10 @@ export const login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
     const user = await User.findOne({ email });
     if (!user) return res.status(400).json({ message: "Invalid Credentials." });
     // never tell client which email or password is incorrect
